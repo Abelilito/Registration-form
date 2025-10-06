ed62014cc345443d39caab22d93c8b2497e53c64
Abel Ilito Limoma <ilitoabel@gmail.com>
2025-10-06 17:28:29 +0200

    Add config gh pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Registration-form",
   css: {
     postcss: {
       plugins: [tailwindcss()],
